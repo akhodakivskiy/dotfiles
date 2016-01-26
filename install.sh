98a05ae8a5afa7aa41a9de7040a6406f442a4800
Anton Khodakivskiy <akhodakivskiy@gmail.com>
2016-01-25 22:44:49 -0800

    tmux config

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@ ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/vim ~/.vim
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 
-echo -e "\nsource ~/dotfiles/bashrc\n" &>> ~/.profile
+echo -e "\nsource ~/dotfiles/bashrc\n" >> ~/.profile
 
+# Vundle for vim
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# TPM for tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
